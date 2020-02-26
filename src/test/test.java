a18ac0a0fd99f909d7b44dd616d384dfde8bf2fd
rzwiruk <42112276+rzwiruk@users.noreply.github.com>
2020-02-26 19:47:50 +0100

    Add files via upload
    Added an operation using bitwise or operator to test its function

--- src/test/test.java
+++ src/test/test.java
@@ -12,7 +12,10 @@ public class test {
 		bool_var = false;
 		char_var = 60;
 		null_string_var = null;
+		
+		
 		System.out.println(char_var);
 		System.out.println(narrowing_casting_int);
+		System.out.println(2 | 3);
 	}
 }
