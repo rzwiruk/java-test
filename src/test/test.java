32201b848789e16256d5145153ba3e46aa05dcb2
rzwiruk <42112276+rzwiruk@users.noreply.github.com>
2020-02-26 19:21:38 +0100

    Add files via upload

--- src/test/test.java
+++ src/test/test.java
@@ -1,7 +1,18 @@
 package test;
 public class test {
 	public static void main(String[] args) {
+		char char_var;
+		boolean bool_var;
+		final String null_string_var;
+		double narrowing_casting_double = 5.72;
+		int narrowing_casting_int;
 		
-		System.out.println("Hello world!");
+		narrowing_casting_int = (int) narrowing_casting_double;
+		
+		bool_var = false;
+		char_var = 60;
+		null_string_var = null;
+		System.out.println(char_var);
+		System.out.println(narrowing_casting_int);
 	}
 }
